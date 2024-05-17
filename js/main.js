897b17c95cb68a33e09c3e7142a610d80a674e70
Will Brady <willdbrady@yahoo.com>
2024-05-17 15:34:03 -0500

    New design.

--- js/main.js
+++ js/main.js
@@ -70,3 +70,76 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active
     })
 })
+
+// PREGUNTAS FRECUENTES
+
+const accordions = document.querySelectorAll(".accordion");
+const openAccordion = (accordion) => {
+    const content = accordion.querySelector(".accordion__content");
+    const arrowButton = accordion.querySelector(".arrowButton");
+    accordion.classList.add("accordion__active");
+    arrowButton.classList.add("arrowButton__active");
+    content.style.maxHeight = content.scrollHeight + "px";
+};
+
+const closeAccordion = (accordion) => {
+    const content = accordion.querySelector(".accordion__content");
+    const arrowButton = accordion.querySelector(".arrowButton");
+    accordion.classList.remove("accordion__active");
+    arrowButton.classList.remove("arrowButton__active");
+    content.style.maxHeight = null;
+};
+
+accordions.forEach((accordion) => {
+    const intro = accordion.querySelector(".accordion__intro");
+    const content = accordion.querySelector(".accordion__content");
+
+    intro.onclick = () => {
+        if (content.style.maxHeight) {
+            closeAccordion(accordion);
+        } else {
+            accordions.forEach((accordion) => closeAccordion(accordion));
+            openAccordion(accordion);
+        }
+    };
+});
+
+//Festivales carousel
+
+const track = document.querySelector('.festivales-carousel__track');
+const cards = Array.from(track.children);
+const nextButton = document.querySelector('.festivales-carousel__button--right');
+const prevButton = document.querySelector('.festivales-carousel__button--left');
+const indicators = document.querySelectorAll('.festivales-carousel__indicator');
+
+const cardWidth = cards[0].getBoundingClientRect().width;
+
+let currentIndex = 0;
+
+const updateIndicators = () => {
+    indicators.forEach((indicator, index) => {
+        indicator.classList.toggle('festivales-carousel__indicator--active', index === Math.floor(currentIndex / 1));
+    });
+};
+
+const moveToCard = (currentIndex) => {
+    const amountToMove = (-cardWidth - 40) * currentIndex;
+    track.style.transform = `translateX(${amountToMove}px)`;
+    updateIndicators();
+};
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < cards.length - 3) {
+        currentIndex++;
+        moveToCard(currentIndex);
+    }
+});
+
+prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+        moveToCard(currentIndex);
+    }
+});
+
+updateIndicators();
